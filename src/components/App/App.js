d405f1c33de4e72d646797805e9ed05c6a37eb4c
rajat2006 <36767918+rajat2006@users.noreply.github.com>
2023-09-05 10:07:36 +0530

    add constants for breakpoint and queries

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
+import { queries } from "../../constants";
 
-import Header from '../Header';
-import ShoeIndex from '../ShoeIndex';
+import Header from "../Header";
+import ShoeIndex from "../ShoeIndex";
 
 const App = () => {
-  const [sortId, setSortId] = React.useState('newest');
+  const [sortId, setSortId] = React.useState("newest");
 
   return (
     <>
@@ -19,6 +20,16 @@ const App = () => {
 
 const Main = styled.main`
   padding: 64px 32px;
+
+  /* @media ${queries.laptopAndSmaller} {
+    background-color: yellow;
+  }
+  @media ${queries.tabletAndSmaller} {
+    background-color: red;
+  }
+  @media ${queries.phoneAndSmaller} {
+    background-color: fuchsia;
+  } */
 `;
 
 export default App;
