65f7ff317b5ee1b5f448568fa73d41131afbf906
rajat2006 <36767918+rajat2006@users.noreply.github.com>
2023-09-23 17:33:50 +0530

    change constant name

--- src/constants.js
+++ src/constants.js
@@ -23,7 +23,7 @@ export const BREAKPOINTS = {
   laptop: 1350,
 };
 
-export const queries = {
+export const QUERIES = {
   phoneAndSmaller: `(max-width: ${BREAKPOINTS.phone / 16}rem)`,
   tabletAndSmaller: `(max-width: ${BREAKPOINTS.tablet / 16}rem)`,
   laptopAndSmaller: `(max-width: ${BREAKPOINTS.laptop / 16}rem)`,
